4426b3cd20020bc25e8d32c9b71549b46b6f8110
LukeWFerguson <lukewferguson@gmail.com>
2021-02-21 11:16:14 -0600

    Training / testing of model complete, but only on a single factor.

--- app.py
+++ app.py
@@ -1,4 +1,46 @@
 import pandas
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import time
+
+# Allows for printing all columns and rows without truncation.
+pandas.set_option('display.max_rows', None)
+pandas.set_option('display.max_columns', None)
+pandas.set_option('display.width', None)
+pandas.set_option('display.max_colwidth', None)
+pandas.options.mode.chained_assignment = None
 
 dataframe = pandas.read_csv('data/kepler_exoplanet_search_results.csv')
-print(dataframe)
+# print(dataframe.head())
+
+# Factorizing text columns.
+dataframe[['koi_disposition_num', 'koi_pdisposition_num']] = dataframe[
+    ['koi_disposition', 'koi_pdisposition']].stack().rank(method='dense').unstack()
+# print(dataframe.head())
+
+train, test = train_test_split(dataframe, random_state=42, shuffle=True)  # Splits into 75% and 25%.
+print('Total dataset: ' + str(len(dataframe)) + ' train dataset count: ' + str(
+    len(train)) + ' test dataset count: ' + str(len(test)) + '.')
+
+# print(train[train.columns.difference(['rowid', 'kepid', 'kepoi_name', 'kepler_name', 'koi_disposition', 'koi_pdisposition', 'koi_tce_delivname'])].head())
+
+model = RandomForestClassifier(max_depth=2, random_state=42)
+print('Model training...')
+start_time = time.time()
+
+# Training without certain columns.
+# train[train.columns.difference(['rowid', 'kepid', 'kepoi_name', 'kepler_name', 'koi_disposition', 'koi_pdisposition', 'koi_tce_delivname'])]
+model.fit(train[['dec']], train['koi_disposition_num'])
+total = time.time() - start_time
+print('Model trained in ' + str(round(total, 2)) + ' seconds.')
+
+print('Predicting test dataset...')
+start_time = time.time()
+# test['prediction'] = model.predict(test[test.columns.difference(['rowid', 'kepid', 'kepoi_name', 'kepler_name', 'koi_disposition', 'koi_pdisposition', 'koi_tce_delivname', 'koi_disposition'])])
+test['prediction'] = model.predict(test[['dec']])
+total = time.time() - start_time
+print('Test dataset predicted in ' + str(round(total, 2)) + ' seconds.')
+
+print(test[['kepoi_name', 'koi_disposition', 'koi_disposition_num', 'prediction']].head())
+print('The model is right ' + str(
+    round((len(test[test.koi_disposition_num == test.prediction]) / len(test) * 100), 2)) + '% of the time.')
